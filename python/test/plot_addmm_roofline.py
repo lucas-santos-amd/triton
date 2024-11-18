db0babd95903d0776e953ae73b3d31594a551b9e
Bruno Mazzotti <bruno.mazzotti@amd.com>
2024-11-18 14:31:46 +0000

    [Roffline plot] Plot two legends

--- python/test/plot_addmm_roofline.py
+++ python/test/plot_addmm_roofline.py
@@ -2,6 +2,7 @@
 
 # -*- coding: utf-8 -*-
 
+import math
 import os
 
 import matplotlib.pyplot as plt
@@ -48,25 +49,34 @@ def get_target_hardware_specs() -> pd.DataFrame:
     return df
 
 
-# Categorical color palette from this website:
-# https://carbondesignsystem.com/data-visualization/color-palettes/#categorical-palettes
-def get_color_palette() -> list[str]:
-    return [
-        "#8a3ffc",  # Purple 60
-        "#33b1ff",  # Cyan 40
-        "#007d79",  # Teal 60
-        "#ff7eb6",  # Magenta 40
-        "#fa4d56",  # Red 50
-        "#fff1f1",  # Red 10
-        "#6fdc8c",  # Green 30
-        "#4589ff",  # Blue 50
-        "#d12771",  # Magenta 60
-        "#d2a106",  # Yellow 40
-        "#08bdba",  # Teal 40
-        "#bae6ff",  # Cyan 20
-        "#ba4e00",  # Orange 60
-        "#d4bbff",  # Purple 30
-    ]
+class ColorPalette:
+    colors: list[str]
+    color_index: int
+
+    def __init__(self):
+        # Categorical color palette from this website:
+        # https://carbondesignsystem.com/data-visualization/color-palettes/#categorical-palettes
+        self.colors = [
+            "#8a3ffc",  # Purple 60
+            "#33b1ff",  # Cyan 40
+            "#007d79",  # Teal 60
+            "#ff7eb6",  # Magenta 40
+            "#fa4d56",  # Red 50
+            "#6fdc8c",  # Green 30
+            "#4589ff",  # Blue 50
+            "#d12771",  # Magenta 60
+            "#d2a106",  # Yellow 40
+            "#08bdba",  # Teal 40
+            "#bae6ff",  # Cyan 20
+            "#ba4e00",  # Orange 60
+            "#d4bbff",  # Purple 30
+        ]
+        self.color_index = 0
+
+    def next_color(self) -> str:
+        color: str = self.colors[self.color_index % len(self.colors)]
+        self.color_index += 1
+        return color
 
 
 def main() -> None:
@@ -74,9 +84,18 @@ def main() -> None:
     shapes_df: pd.DataFrame = get_target_shapes()
     hw_df: pd.DataFrame = get_target_hardware_specs()
 
+    # Set chart size:
+    fig_width: float
+    fig_height: float
+    fig_width, fig_height = plt.rcParams["figure.figsize"]
+    fig_scale: float = math.sqrt(2)
+    fig_width *= fig_scale
+    fig_height *= fig_scale
+    fig = plt.figure(figsize=(fig_width, fig_height))
+    ax = fig.add_axes([0.1, 0.1, 0.6, 0.75])
+
     # Set chart title and axes' titles:
     # TODO: Improve formatting of titles.
-    fig, ax = plt.subplots()
     ax.set(
         title="Phantom addmm Kernel Roofline",
         xlabel="Arithmetic Intensity (Op / B)",
@@ -87,33 +106,54 @@ def main() -> None:
     max_x: float = 1.05 * max(shapes_df["arith_intensity"].max(), hw_df["ops_bytes_ratio"].max())
     x: npt.NDArray = np.arange(0, max_x, 0.1)
 
+    # Get color palette:
+    color_palette: ColorPalette = ColorPalette()
+
     # Plot rooflines for each hardware:
-    color_palette: list[str] = get_color_palette()
-    for i, hw in hw_df.iterrows():
+    rooflines: list = []
+    for _, hw in hw_df.iterrows():
         y: npt.NDArray = np.minimum(hw["comp_perf_tops"], hw["mem_bandwidth_tbs"] * x)
-        color: str = color_palette[i % len(color_palette)]
-        ax.plot(x, y, label=hw["desc"], color=color)
+        color: str = color_palette.next_color()
+        roofline, = ax.plot(x, y, label=hw["desc"], color=color)
+        rooflines.append(roofline)
         ax.vlines(
             x=hw["ops_bytes_ratio"],
             ymin=0,
             ymax=hw["comp_perf_tops"],
             color=color,
-            linestyles="--",
+            linestyles="dotted",
         )
+
+    # Plot legend for rooflines:
+    rooflines_legend = ax.legend(handles=rooflines, title="Hardware Rooflines", loc="upper left",
+                                 bbox_to_anchor=(1.05, 1))
+    ax.add_artist(rooflines_legend)
 
     # Plot arithmetic intensity of each shape:
     max_y: float = ax.get_ylim()[1]
+    shapes: list = []
     for _, shape in shapes_df.iterrows():
         # TODO:
-        # * Use different colors for each shape.
         # * Improve shape legend text, i.e. display (M, N, K).
-        ax.vlines(x=shape["arith_intensity"], ymin=0, ymax=max_y, label=shape["desc"])
+        shape = ax.vlines(
+            x=shape["arith_intensity"],
+            ymin=0,
+            ymax=max_y,
+            label=shape["desc"],
+            color=color_palette.next_color(),
+            linestyles="dashed",
+        )
+        shapes.append(shape)
 
-    # Plot legend:
-    # TODO:
-    # * Plot legend outside main chart area.
-    # * Plot two distinct legends, one for rooflines other for shapes.
-    ax.legend(title="Legend")
+    # Plot lengend for shapes:
+    roofline_legend_bbox = rooflines_legend.get_window_extent().transformed(fig.transFigure.inverted())
+    shapes_legend = ax.legend(
+        handles=shapes,
+        title="Shapes",
+        loc="upper left",
+        bbox_to_anchor=(1.05, roofline_legend_bbox.y0),
+    )
+    ax.add_artist(shapes_legend)
 
     # Save chart to image file:
     plot_img_file_name: str = os.path.splitext(os.path.basename(__file__))[0] + ".png"
