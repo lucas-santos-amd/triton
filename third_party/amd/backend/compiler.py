b049e8a102f40f8257d666c65bbaf857b41e714d
Bruno Mazzotti <bruno.mazzotti@amd.com>
2024-12-04 20:26:43 +0000

    Filter kernel name before injecting assembly

--- third_party/amd/backend/compiler.py
+++ third_party/amd/backend/compiler.py
@@ -358,13 +358,16 @@ class HIPBackend(BaseBackend):
         metadata["name"] = names[0]
         # llvm -> hsaco
         amdgcn = llvm.translate_to_asm(src, amd.TARGET_TRIPLE, options.arch, '', [], options.enable_fp_fusion, False)
-        if "AMD_INSERT_AMDGCN" in os.environ.keys():
-            insert_module_path = str(os.environ["AMD_INSERT_AMDGCN"])
+        if "AMD_INSERT_AMDGCN_FILE" in os.environ.keys():
+            insert_module_path = str(os.environ["AMD_INSERT_AMDGCN_FILE"])
             if not os.path.exists(insert_module_path):
                 raise RuntimeError(f'cannot find amdgcn file to insert. Given: `{insert_module_path}`')
             with open(insert_module_path, "r") as file:
                 file_content = file.readlines()
-            amdgcn = ''.join(file_content)
+            file_content = ''.join(file_content)
+            if "AMD_INSERT_AMDGCN_KERNEL" in os.environ.keys() and str(
+                    os.environ["AMD_INSERT_AMDGCN_KERNEL"]) == metadata["name"]:
+                amdgcn = file_content
         if os.environ.get("AMDGCN_ENABLE_DUMP", "0") == "1":
             print("// -----// AMDGCN Dump //----- //")
             print(amdgcn)
