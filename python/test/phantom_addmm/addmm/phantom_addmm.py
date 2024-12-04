40da1f42be41ce0637ad233b607f532a378bbc8e
Bruno Mazzotti <bruno.mazzotti@amd.com>
2024-12-04 18:27:56 +0000

    [Org] Code cleanup and organization on 1st `addmm` kernel

--- python/test/phantom_addmm/addmm/phantom_addmm.py
+++ python/test/phantom_addmm/addmm/phantom_addmm.py
@@ -1,88 +1,54 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#!/usr/bin/env python
 
-#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-# pyre-unsafe
+# BEGIN IMPORTS >>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-from typing import List
-
-import torch
-
-# @manual=//triton:triton
-import triton
-
-# @manual=//triton:triton
-import triton.language as tl
-import argparse
+import enum
 import sys
+
 import pytest
+import torch
+from torch import Tensor
 
-ENABLE_FULL_TURNING_SPACE = True
+import triton
+import triton.language as tl
+
+# END IMPORTS <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+# BEGIN UTILITIES >>>>>>>>>>>>>>>>>>>>>>>>>>
+
+# Get target shapes:
 
 
-def get_mm_configs() -> List[triton.Config]:
-    if torch.version.hip:
-        if ENABLE_FULL_TURNING_SPACE:
-            block_m_range = [32, 64, 128, 256]
-            block_n_range = [32, 64, 128, 256]
-            block_k_range = [32, 64]
-            group_m_range = [4, 8]
-            matrix_instr_nonkdim_range = [16]
-            waves_per_eu_range = [0]
-            kpack_range = [1, 2]
-            num_warps_range = [4, 8]
-            num_stage_range = [2]
-            # else:
-            #     block_m_range = [256]
-            #     block_n_range = [256]
-            #     block_k_range = [32]
-            #     group_m_range = [8]
-            #     matrix_instr_nonkdim_range = [16]
-            #     waves_per_eu_range = [0]
-            #     kpack_range = [2]
-            #     num_warps_range = [8]
-            #     num_stage_range = [0]
+def get_shapes() -> list[tuple[int, int, int]]:
+    return [
+        (20196, 512, 1536),
+        (171792, 512, 1536),
+        (173318, 512, 1536),
+        # M is aligned to 256 for above shapes
+        # (20224, 512, 1536),
+        # (172032, 512, 1536),
+        # (173568, 512, 1536),
+    ]
 
+
+# Get tuning configs:
+# (both kernels use the same configs)
+
+
+class HipTuningSpace(enum.Enum):
+    FULL = enum.auto()
+    REDUCED = enum.auto()
+    CHERRY_PICKED = enum.auto()
+
+
+def get_configs(hip_tuning_space: HipTuningSpace = HipTuningSpace.CHERRY_PICKED) -> list[triton.Config]:
+    if torch.version.hip:  # HIP configs
+        if hip_tuning_space == HipTuningSpace.CHERRY_PICKED:
             return [
                 triton.Config(
                     {
-                        "BLOCK_M": block_m,
-                        "BLOCK_N": block_n,
-                        "BLOCK_K": block_k,
-                        "GROUP_M": group_m,
-                        "matrix_instr_nonkdim": matrix_instr_nonkdim,
-                        "waves_per_eu": waves_per_eu,
-                        "kpack": kpack,
-                    },
-                    num_stages=num_stages,
-                    num_warps=num_warps,
-                )
-                for block_m in block_m_range
-                for block_n in block_n_range
-                for block_k in block_k_range
-                for group_m in group_m_range
-                for matrix_instr_nonkdim in matrix_instr_nonkdim_range
-                for waves_per_eu in waves_per_eu_range
-                for kpack in kpack_range
-                for num_stages in num_stage_range
-                for num_warps in num_warps_range
-            ]
-        else:
-            configs = [
-                triton.Config(
-                    {
                         "BLOCK_M": 256, "BLOCK_N": 256, "BLOCK_K": 32, "GROUP_M": 4, "matrix_instr_nonkdim": 16,
                         "waves_per_eu": 0, "kpack": 1
                     }, num_warps=8, num_stages=2),
@@ -102,96 +68,68 @@ def get_mm_configs() -> List[triton.Config]:
                         "waves_per_eu": 0, "kpack": 1
                     }, num_warps=8, num_stages=2),
             ]
-            return configs
-    else:
+        block_m_range: list[int]
+        block_n_range: list[int]
+        block_k_range: list[int]
+        group_m_range: list[int] = [4, 8]
+        matrix_instr_nonkdim_range: list[int] = [16]
+        waves_per_eu_range: list[int] = [0]
+        kpack_range: list[int] = [1, 2]
+        num_warps_range: list[int]
+        num_stages_range: list[int] = [2]
+        if hip_tuning_space == HipTuningSpace.FULL:
+            block_m_range = [32, 64, 128, 256]
+            block_n_range = [32, 64, 128, 256]
+            block_k_range = [32, 64]
+            num_warps_range = [4, 8]
+        if hip_tuning_space == HipTuningSpace.REDUCED:
+            block_m_range = [256]
+            block_n_range = [256]
+            block_k_range = [32]
+            num_warps_range = [8]
         return [
             triton.Config(
                 {
-                    "BLOCK_M": 32,
-                    "BLOCK_N": 64,
-                    "BLOCK_K": 32,
-                    "GROUP_M": 8,
+                    "BLOCK_M": block_m,
+                    "BLOCK_N": block_n,
+                    "BLOCK_K": block_k,
+                    "GROUP_M": group_m,
+                    "matrix_instr_nonkdim": matrix_instr_nonkdim,
+                    "waves_per_eu": waves_per_eu,
+                    "kpack": kpack,
                 },
-                num_stages=5,
-                num_warps=2,
-            ),
-            triton.Config(
-                {
-                    "BLOCK_M": 128,
-                    "BLOCK_N": 256,
-                    "BLOCK_K": 64,
-                    "GROUP_M": 8,
-                },
-                num_stages=3,
-                num_warps=8,
-            ),
-            triton.Config(
-                {
-                    "BLOCK_M": 64,
-                    "BLOCK_N": 256,
-                    "BLOCK_K": 32,
-                    "GROUP_M": 8,
-                },
-                num_stages=4,
-                num_warps=4,
-            ),
-            triton.Config(
-                {
-                    "BLOCK_M": 128,
-                    "BLOCK_N": 128,
-                    "BLOCK_K": 32,
-                    "GROUP_M": 8,
-                },
-                num_stages=4,
-                num_warps=4,
-            ),
-            triton.Config(
-                {
-                    "BLOCK_M": 128,
-                    "BLOCK_N": 64,
-                    "BLOCK_K": 32,
-                    "GROUP_M": 8,
-                },
-                num_stages=4,
-                num_warps=4,
-            ),
-            triton.Config(
-                {
-                    "BLOCK_M": 64,
-                    "BLOCK_N": 128,
-                    "BLOCK_K": 32,
-                    "GROUP_M": 8,
-                },
-                num_stages=4,
-                num_warps=4,
-            ),
-            triton.Config(
-                {
-                    "BLOCK_M": 128,
-                    "BLOCK_N": 32,
-                    "BLOCK_K": 32,
-                    "GROUP_M": 8,
-                },
-                num_stages=4,
-                num_warps=4,
-            ),
-            triton.Config(
-                {
-                    "BLOCK_M": 64,
-                    "BLOCK_N": 32,
-                    "BLOCK_K": 32,
-                    "GROUP_M": 8,
-                },
-                num_stages=5,
-                num_warps=2,
-            ),
+                num_stages=num_stages,
+                num_warps=num_warps,
+            )
+            for block_m in block_m_range
+            for block_n in block_n_range
+            for block_k in block_k_range
+            for group_m in group_m_range
+            for matrix_instr_nonkdim in matrix_instr_nonkdim_range
+            for waves_per_eu in waves_per_eu_range
+            for kpack in kpack_range
+            for num_stages in num_stages_range
+            for num_warps in num_warps_range
+        ]
+    else:  # CUDA configs
+        return [
+            triton.Config({"BLOCK_M": 32, "BLOCK_N": 64, "BLOCK_K": 32, "GROUP_M": 8}, num_stages=5, num_warps=2),
+            triton.Config({"BLOCK_M": 128, "BLOCK_N": 256, "BLOCK_K": 64, "GROUP_M": 8}, num_stages=3, num_warps=8),
+            triton.Config({"BLOCK_M": 64, "BLOCK_N": 256, "BLOCK_K": 32, "GROUP_M": 8}, num_stages=4, num_warps=4),
+            triton.Config({"BLOCK_M": 128, "BLOCK_N": 128, "BLOCK_K": 32, "GROUP_M": 8}, num_stages=4, num_warps=4),
+            triton.Config({"BLOCK_M": 128, "BLOCK_N": 64, "BLOCK_K": 32, "GROUP_M": 8}, num_stages=4, num_warps=4),
+            triton.Config({"BLOCK_M": 64, "BLOCK_N": 128, "BLOCK_K": 32, "GROUP_M": 8}, num_stages=4, num_warps=4),
+            triton.Config({"BLOCK_M": 128, "BLOCK_N": 32, "BLOCK_K": 32, "GROUP_M": 8}, num_stages=4, num_warps=4),
+            triton.Config({"BLOCK_M": 64, "BLOCK_N": 32, "BLOCK_K": 32, "GROUP_M": 8}, num_stages=5, num_warps=2),
         ]
 
 
-@triton.autotune(
-    configs=get_mm_configs(),
-    key=["M", "N", "K"],
-)
+# END UTILITIES <<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+# BEGIN BASELINE KERNEL >>>>>>>>>>>>>>>>>>>>
+
+
+@triton.autotune(configs=get_configs(), key=["M", "N", "K"])
 @triton.jit
 def _addmm_fwd_baseline(
     x_ptr,
@@ -264,13 +202,49 @@ def _addmm_fwd_baseline(
     tl.store(z_ptrs, z, mask=z_mask)
 
 
-@triton.autotune(
-    configs=get_mm_configs(),
-    key=["M", "N", "K"],
-)
-@triton.heuristics({
-    'EVEN_K': lambda args: args['K'] % (args['BLOCK_K']) == 0,
-})
+class _AddMmBaselineFunction(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, x: Tensor, w: Tensor, y: Tensor, z: Tensor) -> Tensor:
+        M, K = x.shape
+        KB, N = w.shape
+        assert K == KB, f"incompatible dimensions {K}, {KB}"
+
+        if M == 0 or N == 0:
+            return z
+
+        def grid(META):
+            return (triton.cdiv(M, META["BLOCK_M"]) * triton.cdiv(N, META["BLOCK_N"]), )
+
+        _addmm_fwd_baseline[grid](
+            x,
+            w,
+            y,
+            z,
+            M,
+            N,
+            K,
+            x.stride(0),
+            x.stride(1),
+            w.stride(0),
+            w.stride(1),
+            y.stride(0),
+            y.stride(1),
+            z.stride(0),
+            z.stride(1),
+            ALLOW_TF32=torch.backends.cuda.matmul.allow_tf32,
+        )
+
+        return z
+
+
+# END BASELINE KERNEL <<<<<<<<<<<<<<<<<<<<<<
+
+# BEGIN OPTIMIZED KERNEL >>>>>>>>>>>>>>>>>>>
+
+
+@triton.autotune(configs=get_configs(), key=["M", "N", "K"])
+@triton.heuristics({'EVEN_K': lambda args: args['K'] % (args['BLOCK_K']) == 0})
 @triton.jit
 def _addmm_fwd_optimized(
     x_ptr,
@@ -293,7 +267,6 @@ def _addmm_fwd_optimized(
     BLOCK_K: tl.constexpr,
     GROUP_M: tl.constexpr,
     ALLOW_TF32: tl.constexpr,
-    # BROADCAST_Y: tl.constexpr,
     EVEN_K: tl.constexpr,
 ):
     pid = tl.program_id(axis=0)
@@ -331,17 +304,10 @@ def _addmm_fwd_optimized(
         w_ptrs = w_ptr + (offs_n[None, :] * stride_wn + offs_k[:, None] * stride_wk)
         x = tl.load(x_ptrs, mask=offs_k[None, :] < K, other=0.)
         w = tl.load(w_ptrs, mask=offs_k[:, None] < K, other=0.)
-        # We accumulate along the K dimension.
         accumulator += tl.dot(x, w)
 
     z = accumulator.to(z_ptr.dtype.element_ty)
     mask_yz = (offs_m[:, None] < M) and (offs_n[None, :] < N)
-    # if False:  # BROADCAST_Y:
-    #     # y is a vector, broadcast to add to z
-    #     y_ptr += pid_n.to(tl.int64) * BLOCK_N * stride_yn
-    #     y_ptrs = y_ptr + stride_yn * offs_n[None, :]
-    #     y = tl.load(y_ptrs, mask=mask_n)
-    # else:
     y_ptrs = y_ptr + stride_ym * offs_m[:, None] + stride_yn * offs_n[None, :]
     y = tl.load(y_ptrs, mask=mask_yz)
     z = z + y
@@ -349,67 +315,15 @@ def _addmm_fwd_optimized(
     tl.store(z_ptrs, z, mask=mask_yz)
 
 
-class _AddMmBaselineFunction(torch.autograd.Function):
-
-    @staticmethod
-    # pyre-ignore[14]
-    def forward(
-        ctx,
-        x: torch.Tensor,
-        w: torch.Tensor,
-        y: torch.Tensor,
-        z: torch.Tensor,
-    ) -> torch.Tensor:
-        M, K = x.shape
-        KB, N = w.shape
-        assert K == KB, f"incompatible dimensions {K}, {KB}"
-
-        # z = torch.empty((M, N), device=x.device, dtype=x.dtype)
-        if M == 0 or N == 0:
-            return z
-
-        def grid(META):
-            return (triton.cdiv(M, META["BLOCK_M"]) * triton.cdiv(N, META["BLOCK_N"]), )
-
-        _addmm_fwd_baseline[grid](
-            x,
-            w,
-            y,
-            z,
-            M,
-            N,
-            K,
-            x.stride(0),
-            x.stride(1),
-            w.stride(0),
-            w.stride(1),
-            y.stride(0),
-            y.stride(1),
-            z.stride(0),
-            z.stride(1),
-            ALLOW_TF32=torch.backends.cuda.matmul.allow_tf32,
-        )
-
-        return z
-
-
 class _AddMmOptimizedFunction(torch.autograd.Function):
 
     @staticmethod
-    # pyre-ignore[14]
-    def forward(
-        ctx,
-        x: torch.Tensor,
-        w: torch.Tensor,
-        y: torch.Tensor,
-        z: torch.Tensor,
-    ) -> torch.Tensor:
+    def forward(ctx, x: Tensor, w: Tensor, y: Tensor, z: Tensor) -> Tensor:
         M, K = x.shape
         KB, N = w.shape
         assert K == KB, f"incompatible dimensions {K}, {KB}"
         K = K - 128
 
-        # z = torch.empty((M, N), device=x.device, dtype=x.dtype)
         if M == 0 or N == 0:
             return z
 
@@ -438,36 +352,19 @@ class _AddMmOptimizedFunction(torch.autograd.Function):
         return z
 
 
-def torch_matmul_ref(x, w, y):
-    return torch.matmul(x, w) + y
+# END OPTIMIZED KERNEL <<<<<<<<<<<<<<<<<<<<<
 
-
-def get_shapes():
-    shapes = [
-        (20196, 512, 1536),
-        (171792, 512, 1536),
-        (173318, 512, 1536),
-        # M is aligned to 256 for above shapes
-        #   (20224, 512, 1536),
-        #   (172032, 512, 1536),
-        #   (173568, 512, 1536),
-    ]
-    return shapes
+# BEGIN BENCHMARK >>>>>>>>>>>>>>>>>>>>>>>>>>
 
 
 @triton.testing.perf_report(
     triton.testing.Benchmark(
-        x_names=['M', 'N', 'K'],  # Argument names to use as an x-axis for the plot
+        x_names=["M", "N", "K"],
         x_vals=get_shapes(),
-        line_arg='provider',  # Argument name whose value corresponds to a different line in the plot
-        # Possible values for `line_arg`
+        line_arg="provider",
         line_vals=['baseline', 'optimized'],
-        # Label name for the lines
         line_names=["Baseline", "Optimized"],
-        # Line styles
-        styles=[('green', '-'), ('blue', '-')],
-        ylabel="TFLOPS",  # Label name for the y-axis
-        plot_name="matmul-performance",  # Name for the plot, used also as a file name for saving the plot.
+        plot_name="matmul-performance",
         args={},
     ))
 def benchmark(M, N, K, provider):
@@ -484,38 +381,26 @@ def benchmark(M, N, K, provider):
 
     if provider == 'baseline':
         ms, min_ms, max_ms = triton.testing.do_bench(lambda: phantom_addmm_baseline(x, w, y, z), quantiles=quantiles)
-        print(f'SIZE: {M},{N},{K}   Best tuning config: ({_addmm_fwd_baseline.best_config})')
+        print(f'SIZE: {M},{N},{K} Best baseline config: ({_addmm_fwd_baseline.best_config})')
     if provider == 'optimized':
         ms, min_ms, max_ms = triton.testing.do_bench(lambda: phantom_addmm_optimized(x_optim, w_optim, y, z),
                                                      quantiles=quantiles)
-        print(f'SIZE: {M},{N},{K}   Best tuning config: ({_addmm_fwd_optimized.best_config})')
+        print(f'SIZE: {M},{N},{K} Best optimized config: ({_addmm_fwd_optimized.best_config})')
     perf = lambda ms: 2 * M * N * K * 1e-12 / (ms * 1e-3)
     return perf(ms), perf(max_ms), perf(min_ms)
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(
-        prog="GEMM tutorial example",
-        allow_abbrev=False,
-    )
+# END BENCHMARK <<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-    parser.add_argument("-v", action='store_true', default=False, help="Print out the best tuning config")
-    args = parser.parse_args()
-
-    return args
+# BEGIN CORRECTNESS TEST  >>>>>>>>>>>>>>>>>>
+# TODO:
+# * Use best config in test.
+# * Compare output of optimized Triton kernel.
+# * Test is failing! FIX IT!
 
 
-def main():
-    # assign to a global verbose var to indicate whether print
-    # best tuning config
-    global verbose
-    args = parse_args()
-    verbose = args.v
-    benchmark.run(show_plots=True, print_data=True)
-
-
-if __name__ == '__main__':
-    sys.exit(main())
+def torch_matmul_ref(x, w, y):
+    return torch.matmul(x, w) + y
 
 
 @pytest.mark.parametrize('m, n, k', get_shapes())
@@ -531,9 +416,25 @@ def test_addmm(m, n, k):
         phantom_addmm = _AddMmBaselineFunction.apply
         out_torch = torch_matmul_ref(x, w, y)
         out_triton = phantom_addmm(x, w, y, z)
-        print(f"M = {m}, N = {n}, K = {k}, best_config = {_addmm_fwd_baseline.best_config}")
-
-        print(f"out_torch = {out_torch}")
-        print(f"out_triton = {out_triton}")
 
         assert torch.allclose(out_triton.to(torch.float32), out_torch.to(torch.float32))
+
+
+# END CORRECTNESS TEST <<<<<<<<<<<<<<<<<<<<<
+
+# BEGIN SCRIPT ENTRY POINT >>>>>>>>>>>>>>>>>
+# TODO:
+# * Add command line parser, get target shape from user.
+# * Add test runner option.
+# * Add benchmark option
+# * Add standalone runner option for trace collection.
+
+
+def main():
+    benchmark.run(show_plots=False, print_data=True)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
+
+# END SCRIPT ENTRY POINT <<<<<<<<<<<<<<<<<<<
